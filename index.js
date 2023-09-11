31de4d44a639767b6a6c3499ccc8b4b8420a72b3
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-11 16:28:23 -0400

    add functions in index.js and --bail flag to mocha

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+
+}
+
+function logShout(string){
+    let stringShout = shout(string)
+    console.log(`${stringShout}`)
+}
+
+function logWhisper(string){
+    let stringWhisper = whisper(string)
+    console.log(`${stringWhisper}`)
+
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+}
+
